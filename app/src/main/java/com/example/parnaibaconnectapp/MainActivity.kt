ca6aadc6327a3f6cb2d5dbe3658c1672d9f05fc0
leandroAlves88 <lalves.ferreira88@gmail.com>
2024-11-03 17:26:07 -0300

    Commit inicial

---
+++ app/src/main/java/com/example/parnaibaconnectapp/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.parnaibaconnectapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.parnaibaconnectapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val webView = binding.webView
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://g1.globo.com/")
+    }
+}
