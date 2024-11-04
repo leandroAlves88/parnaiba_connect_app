a7f152242ffdac1a92a7f5286ce8feab3c61b48d
leandroAlves88 <lalves.ferreira88@gmail.com>
2024-11-04 20:59:27 -0300

    ajuste webview

--- app/src/main/java/com/example/parnaibaconnectapp/MainActivity.kt
+++ app/src/main/java/com/example/parnaibaconnectapp/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.parnaibaconnectapp
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,13 +14,17 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val webView = binding.webView
+        webView.webViewClient = WebViewClient()
         webView.settings.javaScriptEnabled = true
         webView.loadUrl("https://parnaibaconnect.onrender.com/")
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
     }
 }
